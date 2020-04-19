504fb184632759231fa531e6e63282c3854a6f0d
GabrielRuiseco <gabriel_alejandro_rc@outlook.com>
2020-04-19 01:22:58 -0500

    android

--- ANDROID/app/src/main/java/com/example/appproject2020/MainActivity.java
+++ ANDROID/app/src/main/java/com/example/appproject2020/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.appproject2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_rgst;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_rgst=(Button)findViewById(R.id.button3);
+        btn_rgst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Register.class));
+            }
+        });
     }
 }
