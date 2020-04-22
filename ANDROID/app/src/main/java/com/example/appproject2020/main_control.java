67c9a598f959f44e60243c7ff491d49e7bb9d3a8
GabrielRuiseco <gabriel_alejandro_rc@outlook.com>
2020-04-21 22:43:35 -0500

    Requests

--- ANDROID/app/src/main/java/com/example/appproject2020/main_control.java
+++ ANDROID/app/src/main/java/com/example/appproject2020/main_control.java
@@ -2,7 +2,9 @@ package com.example.appproject2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -22,22 +24,55 @@ import java.util.Map;
 public class main_control extends AppCompatActivity {
 
     private RequestQueue queue;
-    Button btn1, btn2, btn3;
-    String datum = "{\"datum\"{\"value\":\"1\"}}";
+    Button btn1, btn2, btn3, btn4;
+    String datum1 = "{\"datum\"{\"value\":\"1\"}}";
+    String datum2 = "{\"datum\"{\"value\":\"0\"}}";
     String headers = "{\"X-AIO-Key\":\"aio_iKmy47YN8W3gH0VGo6NIICkBy63A\"}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_control);
-        btn1 = (Button) findViewById(R.id.button);
-        btn2 = (Button) findViewById(R.id.button2);
-        btn3 = (Button) findViewById(R.id.button3);
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn3 = findViewById(R.id.button4);
         queue = Volley.newRequestQueue(this);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mix(1, datum1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mix(2, datum1);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mix(3, datum1);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mix(1, datum2);
+                mix(2, datum2);
+                mix(3, datum2);
+            }
+        });
+
     }
 
-    private void mix1(){
-        String url = "http://127.0.0.1:8000/";
+    private void mix(int btn, final String datum) {
+        String url = "https://io.adafruit.com/api/v2/gabriel_rc/feeds/project.mix" + btn + "feed/data";
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
@@ -55,12 +90,12 @@ public class main_control extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("body", datum );
-                params.put("headers", headers );
+                params.put("body", datum);
+                params.put("headers", headers);
                 return params;
             }
         };
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
-       }
+    }
 }
